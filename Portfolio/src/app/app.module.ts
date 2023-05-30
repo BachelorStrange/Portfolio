2c6508f910bf3a46b46ba9fe964d5b74b49ee4ef
dennis <dennis@supergrobi.home>
2023-05-30 21:55:41 +0200

    added nginx-configuration

--- Portfolio/src/app/app.module.ts
+++ Portfolio/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjectsListComponent } from './projects-list/projects-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectsListComponent
   ],
   imports: [
     BrowserModule,
