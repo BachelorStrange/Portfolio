817d7fae86c558cfcbe6f5d05f124ce1119da534
dennis <dennis@supergrobi.home>
2023-06-03 22:14:36 +0200

    added line and changed background color

--- Portfolio/src/app/app.component.ts
+++ Portfolio/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver,Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import { ProjectRegisterComponent } from './project-register/project-register.component';
 
 @Component({
   selector: 'app-root',
@@ -25,7 +26,8 @@ export class AppComponent implements OnInit {
     .observe([Breakpoints.HandsetPortrait])
     .subscribe((state: BreakpointState) => {
       console.log(state);
-    if (state.matches) {
+      this.drawBackgroundLine();
+    if (!state.matches) {
     console.log(
     'This is the Handset Portrait point at max-width: 599.98 px and portrait orientation.'
     );
@@ -33,5 +35,38 @@ export class AppComponent implements OnInit {
     console.log(this._shapeSelector);
     }
     });
+    }
+
+  
+    drawBackgroundLine(){
+      const svg1 = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+      svg1.setAttribute('id','svg');
+
+      const line = document .createElementNS("http://www.w3.org/2000/svg", "line");
+      line.setAttribute('stroke-width', '1px');
+      line.setAttribute('stroke', '#FFFFFF');
+      line.setAttribute('marker-end', 'url(#triangle)');
+      //line.setAttribute('stroke-dasharray','8');
+
+      svg1.appendChild(line);
+      
+      document.body.appendChild(svg1);
+
+      svg1.style.position='absolute';
+      svg1.style.top='0';
+      svg1.style.left='0';
+      svg1.style.width='100%';
+      svg1.style.height='100vh';
+      svg1.style.zIndex='-2';
+
+      const x1 = 100;
+      const x2 = 600;
+      const y1 = 500;
+      const y2 = 500;
+
+      line.setAttribute( 'x1', `${x1}` );
+      line.setAttribute( 'x2', `${x2}` );
+      line.setAttribute( 'y1', `${y1}` );
+      line.setAttribute( 'y2', `${y2}` );
     }
 }
